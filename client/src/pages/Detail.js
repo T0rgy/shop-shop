d0351e0a7b7d5fcf29c850fd21d91abad0322208
torgyLabtop <jctorge@gmail.com>
2022-07-21 15:28:14 -0500

    22.4

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { from, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 
 import { useStoreContext } from '../utils/GlobalState';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 import Cart from '../components/Cart';
 import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY, ADD_TO_CART, UPDATE_PRODUCTS, } from '../utils/actions';
-import CartItem from '../components/CartItem';
+import { idbPromise } from '../utils/helpers';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -31,8 +31,9 @@ function Detail() {
     } else {
       dispatch({
         type: ADD_TO_CART,
-        product: { ...currentProduct, purchaseQuantity: 1 }
+        product: { ...currentProduct, purchaseQuantity: 1 },
     });
+    idbPromise('cart', 'put', { ...currentProduct, purchaseQuantity: 1 });
   }
   };
 
@@ -41,18 +42,35 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
+
+    idbPromise('cart', 'delete', { ...currentProduct });
   };
 
   useEffect(() => {
+    // global store
     if (products.length) {
       setCurrentProduct(products.find((product) => product._id === id));
-    } else if (data) {
+    } // retrieved from server 
+    else if (data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products,
       });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [products, data, dispatch, id]);
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('product', 'get').then((indexedProducts) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
